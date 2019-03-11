b32fe8c1813356082a00ba2972a0d0a7ad74def0
Mainor Miranda <mainor.miranda@bvmedia.cr>
2019-03-10 20:38:52 -0600

    first slide from bottom

--- slide/js/main.js
+++ slide/js/main.js
@@ -1,1 +1,31 @@
-
+$( document ).ready(function() {
+    //
+    /*$(".footer").swipe({
+        //Generic swipe handler for all directions
+        swipe:function(event, direction, distance, duration, fingerCount, fingerData) {
+            if( direction == "up" )
+            {
+                $( ".footer" ).animate({ "top": "35em" }, 300 );
+            }
+            else if( direction == "down" )
+            {
+                $( ".footer" ).animate({ "top": "49em" }, 300 );
+            }
+            //$(this).text("You swiped " + direction );  
+        }
+    });*/
+    console.log("it begins!");
+    var isFooterExpanded = false;
+    $(".footer").on( "click", function() {
+        if( isFooterExpanded ) {
+            console.log("line 21");
+            $( ".footer" ).animate({ "bottom": "-11em" }, 300 );
+            isFooterExpanded = false;
+        }
+        else {
+            console.log("line 26");
+            $( ".footer" ).animate({ "bottom": "0" }, 300 );
+            isFooterExpanded = true;
+        }
+    });
+});
